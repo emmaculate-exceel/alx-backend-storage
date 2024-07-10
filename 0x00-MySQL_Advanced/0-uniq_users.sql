0be423a5549d4be8f1566972cc7346b3acfd9908
emmaculate-exceel <emmaculate2exceel@live.com>
2024-07-10 19:17:47 +0100

    create table

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,8 +1,8 @@
 -- Create a table called users following some requirement
 
 CREATE TABLE IF NOT EXISTS users (
-  id INT NOT NULL ,
-  email VARCHAR(255) NOT NULL,
+  id INT PRIMARY KEY AUTOINCREMENT,
+  email VARCHAR(255) NOT NULL UNIQUE,
   name VARCHAR(255),
   PRIMARY KEY (id)
 );
