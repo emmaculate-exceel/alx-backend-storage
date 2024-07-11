994ccc5046c51ba3cdfef0b897aa2ba563fe1d6a
emmaculate-exceel <emmaculate2exceel@live.com>
2024-07-11 14:44:20 +0100

    update store

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,10 +1,18 @@
 -- Buy buy buy
 
+DELIMITER $$
 CREATE TRIGGER decrease_item_quantity
 AFTER INSERT ON orders
-FOR EACH ROW
-BEGIN
-  UPDATE items
-  SET quantity = quantity - NEW.quantity
-  WHERE item_id = NEW.item_id
-END ;
+FOR EACH ROW BEGIN
+  DECLARE new_quantity INT;
+
+  SELECT quantity INTO new_quantity FROM items WHERE item_id = NEW.item_id;
+
+  IF new_quantity >= NEW.quantity THEN
+    UPDATE items SET quantity = quantity - NEW.quantity WHERE item_id = NEW.item_id;
+  ELSE
+    SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Insufficient stock for item';
+  END IF;
+END $$
+
+DELIMITER ;
