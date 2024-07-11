a44ea46fb39a9015e7da44b89405fcf79b7939b1
emmaculate-exceel <emmaculate2exceel@live.com>
2024-07-11 14:13:36 +0100

    glam rock

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,16 +1,11 @@
--- Buy buy buy
-DELIMITER //
+-- glam rocks
 
-CREATE TRIGGER decrease_quantity_after_order
-AFTER INSERT ON mydatabase.orders FOR EACH ROW 
-BEGIN
-    DECLARE item_id INT;
-    DECLARE order_quantity INT;
-    
-    SELECT NEW.item_id, NEW.quantity INTO item_id, order_quantity;
-
-    UPDATE mydatabase.items
-    SET quantity = quantity - order_quantity
-    WHERE id = item_id;
-END //
-DELIMITER ;
+SELECT
+    band_name,
+    IFNULL(YEAR(split) - YEAR(formed), YEAR(CURDATE()) - YEAR(formed)) AS lifespan
+FROM
+    bands
+WHERE
+    main_style = 'Glam rock'
+ORDER BY
+    lifespan DESC;
