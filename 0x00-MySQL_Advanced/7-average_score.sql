aad31bdd2caf811d7d942c64b509e9274b28a5b6
emmaculate-exceel <emmaculate2exceel@live.com>
2024-07-12 05:11:27 +0100

    avg_score

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,29 +1,23 @@
--- a script that stored the procedure of average users
-
-DELIMITER //
-
-CREATE PROCEDURE ComputeAverageScoreForUser(
-    IN user_id INT
-)
+-- Creates a stored procedure ComputeAverageScoreForUser that
+-- computes and stores the average score for a student.
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+DELIMITER $$
+CREATE PROCEDURE ComputeAverageScoreForUser (user_id INT)
 BEGIN
-    DECLARE avg_score DECIMAL(10, 2);
+    DECLARE total_score INT DEFAULT 0;
+    DECLARE projects_count INT DEFAULT 0;
 
-    -- Compute the average score
-    SELECT AVG(score) INTO avg_score
-    FROM scores
-    WHERE user_id = user_id;
+    SELECT SUM(score)
+        INTO total_score
+        FROM corrections
+        WHERE corrections.user_id = user_id;
+    SELECT COUNT(*)
+        INTO projects_count
+        FROM corrections
+        WHERE corrections.user_id = user_id;
 
-    -- Update or insert the average score into the users table
     UPDATE users
-    SET average_score = avg_score
-    WHERE id = user_id;
-    
-    -- If the user has no scores, set average_score to NULL
-    IF avg_score IS NULL THEN
-        UPDATE users
-        SET average_score = NULL
-        WHERE id = user_id;
-    END IF;
-END //
-
+        SET users.average_score = IF(projects_count = 0, 0, total_score / projects_count)
+        WHERE users.id = user_id;
+END $$
 DELIMITER ;
