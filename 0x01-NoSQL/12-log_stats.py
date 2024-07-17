a7eb5043d127f7e5d0e0326bb2494c0745132b16
emmaculate-exceel <emmaculate2exceel@live.com>
2024-07-17 05:22:15 +0100

    nginx_log

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -6,7 +6,7 @@ from pymongo import MongoClient
 
 
 if __name__ == '__main__':
-    client_con = MongoClient()
+    client_con = MongoClient('mongodb://127.0.0.1:27017')
     nginx_col = client_con.logs.nginx
     nginx_count = nginx_col.count_documents({})
     print(f'{nginx_count} logs')
