3591465a57750223bf3e05e3e2a1a7770a79e3bf
emmaculate-exceel <emmaculate2exceel@live.com>
2024-07-17 04:34:55 +0100

    nginx_log

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" nginx logs """
+from pymongo import MongoClient
+
+if __name__ == '__main__':
+    client_con = MongoClient()
+    nginx_col = client_con.logs.nginx.count_documents({})
+    print(f"{nginx_col} logs")
+    print("Methods:")
+
+    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+        count = nginx_col.count_documents({'method': method})
+        print(f'{\tmethod {method}: {count}')
+
+    nb_counts = nginx_col.count_documents({'method': 'GET'},
+                                          {'path': '/status'})
+    print(f'{nb_counts} status check')
