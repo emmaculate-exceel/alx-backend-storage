9e4871df2bdb9d213a435a410d3b0a9b5d106fbb
emmaculate-exceel <emmaculate2exceel@live.com>
2024-07-17 02:57:16 +0100

    find.topic

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" list of schools in the mongodb collection """
+
+
+def schools_by_topic(mongo_collection, topic):
+    """ schools collection """
+    return mongo_collection.find().topic(topic)
