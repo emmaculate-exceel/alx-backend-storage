c815ecb204958681ccf5244ae0db5abb764ba9ee
emmaculate-exceel <emmaculate2exceel@live.com>
2024-07-17 02:42:31 +0100

    update mongodb

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -4,4 +4,4 @@
 
 def update_topics(mongo_collection, name, topics):
     """ updating collection """
-    mongo_collection.update_many({name: name}, {$set: {topics: topics}})
+    return mongo_collection.update_many({name: name}, {$set: {topics: topics}})
