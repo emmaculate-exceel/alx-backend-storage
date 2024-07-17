c7a04d84bea984993fe13d0e614b11f3e7e321a5
emmaculate-exceel <emmaculate2exceel@live.com>
2024-07-17 02:10:30 +0100

    find all

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" listing all documents in a collection in mongodb """
+
+
+def list_all(mongo_collection):
+    """ function to list all collection """
+    return mongo.collection.find()
